06ea3ad81b7482bc84bef0dff365cdb9e3341cc2
Bobo1108 <kaktuserro@gmail.com>
2026-01-19 20:56:45 +0200

    commit 3

--- server/app.js
+++ server/app.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const cors = require('cors');
-const morgan = require('morgan'); 
+const morgan = require('morgan');
+const admin = require('firebase-admin');
+const serviceAccount = require('./serviceAccountKey.json'); 
 require('dotenv').config();
 
 
@@ -14,7 +16,26 @@ app.get('/', (req, res) => {
   res.send('Serverul HoopLab este pornit și gata de baschet!');
 });
 
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount)
+});
+const db = admin.firestore();
+
+app.get('/test-db', async (req, res) => {
+  try {
+    const testRef = db.collection('test').doc('hooplab');
+    await testRef.set({
+      mesaj: "Conexiunea merge",
+      data: new Date()
+    });
+    res.send("Succes! Verifica consola in Firebase");
+  } catch (error) {
+    res.status(500).send("Eroare: " + error.message);
+  }
+});
+
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`[HoopLab] Serverul rulează pe http://localhost:${PORT}`);
 });
+
